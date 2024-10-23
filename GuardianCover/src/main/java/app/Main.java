54d708ffc15dda45e1dfe84c782a666cef50b779
zoolotoui <chag.med.amine@gmail.com>
2024-10-23 14:29:02 +0100

    stat: working on db connection

--- GuardianCover/src/main/java/app/Main.java
+++ GuardianCover/src/main/java/app/Main.java
@@ -1,11 +1,32 @@
 package app;
 
-import app.config.JPAconfig;
+import app.config.JPApersistenceConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
+        // Create Spring application context
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JPApersistenceConfig.class);
 
+        // Get the DataSource bean
+        DataSource dataSource = context.getBean(DataSource.class);
+
+        // Test the connection
+        try (Connection connection = dataSource.getConnection()) {
+            if (connection != null && !connection.isClosed()) {
+                System.out.println("Connection successful!");
+            } else {
+                System.out.println("Failed to establish connection.");
+            }
+        } catch (SQLException e) {
+            System.out.println("SQLException occurred: " + e.getMessage());
+        }
+
+        // Close the Spring context
+        context.close();
     }
 }
--- GuardianCover/src/main/java/app/config/JPApersistenceConfig.java
+++ GuardianCover/src/main/java/app/config/JPApersistenceConfig.java
@@ -1,41 +1,47 @@
 package app.config;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.beans.BeanProperty;
 import java.util.Properties;
 
 @Configuration
-@EnableTransactionManagement
+@EnableJpaRepositories("app")
+@ComponentScan("app")
 public class JPApersistenceConfig {
 
-    private final DataSource dataSource;
 
-    public JPApersistenceConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    @Bean
+    public DataSource dataSource(){
+        HikariDataSource hikari = new HikariDataSource();
+        hikari.setJdbcUrl("jdbc:postgresql://localhost:5432/guardian");
+        hikari.setUsername("postgres");
+        hikari.setPassword("1360");
+        hikari.setDriverClassName("org.postgresql.Driver");
+        return hikari;
+
+
     }
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
         LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource);
-        em.setPackagesToScan("app.entity");
+        em.setDataSource(dataSource());
+        em.setPackagesToScan("app");
         JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         em.setJpaVendorAdapter(vendorAdapter);
-        em.setJpaProperties(additionalProperties());
         return em;
 
 
     }
-    Properties additionalProperties() {
-        Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
 
-        return properties;
-    }
+
 }
